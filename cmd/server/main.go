006368caa7b8599aa2b819a3625615e897778efc
jon.lasley@gmail.com <jon.lasley@gmail.com>
2022-11-29 16:58:22 -0700

    `preact-cli create` and Go server
    `preact-cli create typescript <whatever>` output, slightly modified
    
    Created server
    
    Supporting files like Taskfile, lint config, etc.

---
+++ cmd/server/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/davidmdm/muxter"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		stop()
+	}()
+
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           ServerHandler(),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		fmt.Printf("starting server on port :%s\n", srv.Addr)
+
+		if err := srv.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				log.Printf("unexpected server error: %s\n", err.Error())
+			}
+		}
+	}()
+
+	<-ctx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("shutting down server")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("unexpected server shutdown error: %s\n", err.Error())
+	}
+}
+
+func ServerHandler() http.Handler {
+	mux := muxter.New()
+
+	mux.Use(Logger(os.Stdout, DefaultLogComposer))
+
+	mux.HandleFunc("/", serveHTML)
+
+	return mux
+}
+
+func serveHTML(w http.ResponseWriter, r *http.Request, _ muxter.Context) {
+	dir := http.FileServer(http.Dir("./public"))
+
+	dir.ServeHTTP(w, r)
+}
